25a4da6406a2131c0833b6ffb64f17b3d5ef1022
golamrabbani551 <grannabi81099@gmail.com>
2023-01-23 12:03:05 +0600

    js commit

---
+++ array.js
@@ -0,0 +1,10 @@
+// Array push pop
+
+
+var foodPrice = ['apple', 'mango', 'banana'];
+foodPrice.push ('orange');
+foodPrice.pop();
+foodPrice.unshift('orange');
+foodPrice.shift();
+
+console.log(foodPrice);
--- first.js
+++ first.js
@@ -1,1 +1,25 @@
-console.log('my first js');
+// // variable
+
+
+// var x = 10;
+// var y = 6;
+// var z = (x + y) * 7;
+
+
+
+
+
+// console.log(z);
+
+
+
+// // Array push pop
+
+
+var foodPrice = ['apple', 'mango', 'banana'];
+// foodPrice.push ('orange');
+// foodPrice.pop();
+// foodPrice.unshift('orange');
+foodPrice.shift();
+
+console.log(foodPrice);
