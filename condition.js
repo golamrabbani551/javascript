846d9307eef7bbd52d4ff9a17bcb2208226d835c
golamrabbani551 <grannabi81099@gmail.com>
2023-01-27 00:29:59 +0600

    while_loop

--- condition.js
+++ condition.js
@@ -1,8 +1,61 @@
-let pocketMoney = 100;
+// let pocketMoney = 120;
 
-if(pocketMoney < 110){
-    console.log(give me a chocolate);
-}
-else{
-    console.log(i will buy later);
+// if(pocketMoney < 110){
+//     console.log('give me a chocolate');
+// }
+// else{
+//     console.log('i will buy later');
+// }
+
+
+
+// let friendAge = 26;
+// if (friendAge <= 5){
+//     console.log('fahim');
+// }
+// else if(friendAge <= 15){
+//     console.log('tanvir')
+// }
+// else if(friendAge <= 20){
+//     console.log('noyon')
+// }
+// else if(friendAge <= 25){
+//     console.log('kajel')
+// }
+// else if(friendAge <= 30){
+//     console.log('munna')
+// }
+// else{
+//     console.log('kamal')
+// }
+
+
+
+// let resultMarks = 75;
+// let totalAttendence = 7.5;
+
+//  if(resultMarks >= 80 && totalAttendence >= 10){
+//     console.log('PASSED NEXT CLASS WITH GPA-5 AND 100% ATTENDENCE');
+//  }
+//  else if(resultMarks >= 70 && totalAttendence >= 8){
+//     console.log('PASSED NEXT CLASS WITH GPA-4.5 AND 80% ATTENDENCE');
+//  }
+//  else if(resultMarks >= 60 && totalAttendence >= 6){
+//     console.log('PASSED NEXT CLASS WITH GPA-4.5 AND 60% ATTENDENCE');
+//  }
+//  else if(resultMarks >= 50 && totalAttendence >= 5){
+//     console.log('PASSED NEXT CLASS WITH GPA-4.5 AND 50% ATTENDENCE');
+//  }
+//  else if(resultMarks >= 40 && totalAttendence >= 4){
+//     console.log('PASSED NEXT CLASS WITH GPA-4.5 AND 40% ATTENDENCE');
+//  }
+// else{
+//     console.log('do not pass in the exam');
+// }
+
+
+let rostGiven = 1;
+while(rostGiven < 8){
+    console.log('pleace give me rost!!!');
+    rostGiven++;
 }
---
+++ while_loop.js
@@ -0,0 +1,12 @@
+// let rostGiven = 0;
+// while(rostGiven < 8){
+//     console.log('pleace give me rost!!!');
+//     rostGiven++;
+// }
+
+
+let rostGiven = 4;
+
+for(let i = 0; i < rostGiven; i++){
+    console.log('give me a roast')
+}
