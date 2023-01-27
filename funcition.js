354277a44bc66970d7f07ee44ca2f74a0e490499
golamrabbani551 <grannabi81099@gmail.com>
2023-01-27 22:05:06 +0600

    funcition

---
+++ funcition.js
@@ -0,0 +1,30 @@
+
+
+
+
+// function myFunc(theArr) {
+//     theArr[0] = 30;
+//   }
+  
+//   const arr = [45];
+  
+//   console.log(arr[0]); // 45
+//   myFunc(arr);
+//   console.log(arr[0]); // 30
+  
+
+// function sum(a, b, c, d, e){
+//     console.log(a, b, c, d, e);
+//     var sum = a + b + c + d + e;
+//     console.log(sum);
+// }
+
+// sum(1, 2, 3, 4, 5);
+
+// function mul(num1, num2, num3,){
+//     console.log(num1, num2, num3);
+//     var mul = num1 * num2 * num3;
+//     console.log(mul);
+// }
+
+// mul(11, 22, 33);
